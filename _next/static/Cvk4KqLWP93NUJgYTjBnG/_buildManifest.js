78e8261224ac2cfbd7255f99c64ebe1633f146e7
Vaala Cat <me@vaala.cat>
2024-01-26 09:46:19 +0800

    brand new blog

---
+++ _next/static/Cvk4KqLWP93NUJgYTjBnG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-6766422533cdb8d2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Cvk4KqLWP93NUJgYTjBnG/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[...slug]","\u002F[year]\u002F[mon]\u002F[date]\u002F[name]","\u002Fposts\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
