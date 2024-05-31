2fadfb6e4530273951f517678ec12b933308fd02
Vaala Cat <me@vaala.cat>
2024-05-31 23:36:06 +0800

    feat: new lichee rv nano blog

---
+++ _next/static/chunks/app/[year]/[mon]/[date]/[name]/page-16fd277953a29fe8.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[227],{4756:function(e,r,t){Promise.resolve().then(t.t.bind(t,7405,23)),Promise.resolve().then(t.t.bind(t,3362,23)),Promise.resolve().then(t.t.bind(t,1122,23)),Promise.resolve().then(t.t.bind(t,6383,23)),Promise.resolve().then(t.bind(t,7181))},7181:function(e,r,t){"use strict";t.r(r),t.d(r,{AutoRedirect:function(){return i}});var n=t(488),o=t(6339),u=t(8912);let i=e=>{let{path:r}=e,t=(0,o.useRouter)();return(0,u.useEffect)(()=>{t.push(r)},[t]),(0,n.jsx)("div",{children:"正在跳转..."})}},6383:function(e,r,t){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),function(e,r){for(var t in r)Object.defineProperty(e,t,{enumerable:!0,get:r[t]})}(r,{suspense:function(){return o},NoSSR:function(){return u}}),t(7023),t(8912);let n=t(6593);function o(){let e=Error(n.NEXT_DYNAMIC_NO_SSR_CODE);throw e.digest=n.NEXT_DYNAMIC_NO_SSR_CODE,e}function u(e){let{children:r}=e;return r}},3568:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(8912),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,t){var n,u={},c=null,_=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(_=r.ref),r)i.call(r,n)&&!f.hasOwnProperty(n)&&(u[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===u[n]&&(u[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:_,props:u,_owner:s.current}}r.Fragment=u,r.jsx=c,r.jsxs=c},488:function(e,r,t){"use strict";e.exports=t(3568)},6339:function(e,r,t){e.exports=t(8404)}},function(e){e.O(0,[397,704,744],function(){return e(e.s=4756)}),_N_E=e.O()}]);
